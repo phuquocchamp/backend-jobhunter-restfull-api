761effcafe927aac39a693011091c369888f3ea9
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-07-30 10:33:39 +0700

    create project structure

---
+++ src/main/java/example/restfullapiapplication/RestfullApiApplication.java
@@ -0,0 +1,18 @@
+package example.restfullapiapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+})
+public class RestfullApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/restfullapiapplication/RestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package example.restfullapiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
